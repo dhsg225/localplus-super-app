42e3e5df6a031908a0fe020a54a758a9c62225fb
dhsg225 <shannon.green.asia@gmail.com>
2025-08-24 21:07:10 +0700

    fix(partner): add vite.config.ts to handle jsx in js files

--- partner/vite.config.ts
+++ partner/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@shared': path.resolve(__dirname, './shared'),
-    },
-  },
+  plugins: [
+    react({
+      include: "**/*.{jsx,tsx,js,ts}",
+    }),
+  ],
+  build: {
+    outDir: 'dist'
+  }
 }) 
