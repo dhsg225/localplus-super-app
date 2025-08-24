590816baf4086d14df86f900043f3c77df10ef43
dhsg225 <shannon.green.asia@gmail.com>
2025-08-24 22:34:02 +0700

    FIX: Correct paths in partner vite.config.ts

--- partner/vite.config.ts
+++ partner/vite.config.ts
@@ -4,22 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      include: "**/*.{jsx,tsx,js,ts}",
-    }),
+    react(),
   ],
   esbuild: {
     loader: 'jsx',
     include: [
-      'src/**/*.js',
-      'src/**/*.jsx',
-      'src/**/*.ts',
-      'src/**/*.tsx',
-      'shared/**/*.js',
-      'shared/**/*.jsx',
-      'shared/**/*.ts',
-      'shared/**/*.tsx',
-    ],
-    exclude: [],
-  },
+      './src/**/*.js',
+      '../shared/**/*.js'
+    ]
+  }
 })
